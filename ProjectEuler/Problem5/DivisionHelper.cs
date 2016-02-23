cfdb4b85571cf0f06092ae33cbe23b7e8d0f6cb8
Daniel Mallott <daniel.mallott@gmail.com>
2016-02-23 13:29:55 -0600

    Project 5 Solution Finder

---
+++ ProjectEuler/Problem5/DivisionHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem5
+{
+    internal static class DivisionHelper
+    {
+        public static bool Test(int numberToTest, int maxDivisor)
+        {
+            bool success = true;
+            ConcurrentBag<int> successfulDivisors = new ConcurrentBag<int>();
+
+            Parallel.For(1, maxDivisor + 1, i =>
+            {
+                if (numberToTest % i == 0)
+                {
+                    successfulDivisors.Add(i);
+                }
+            });
+            
+            if(successfulDivisors.Count < maxDivisor)
+            {
+                success = false;
+            }
+
+            return success;
+        }
+    }
+}
---
+++ ProjectEuler/Problem5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool shouldContinue = true;
+            int testingValue = 2520;
+            int maxDivisor = 20;
+
+            while(shouldContinue)
+            {
+                Console.WriteLine("Testing: {0}", testingValue);
+                shouldContinue = !(DivisionHelper.Test(testingValue, maxDivisor));
+                if(shouldContinue)
+                {
+                    testingValue += 10;
+                }
+            }
+
+            Console.WriteLine("The number: {0} is evenly divisible by all integers from 1 to {1}", testingValue, maxDivisor);
+            Console.Read();
+        }
+    }
+}
