68b707acc2f97fa26d86d6b61b049f10b1e8a886
Daniel Mallott <daniel.mallott@gmail.com>
2016-02-24 15:43:37 -0600

    Problem 10 Solution

---
+++ ProjectEuler/Problem10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathHelper;
+
+namespace Problem10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get all primes under 2,000,000
+            ConcurrentBag<long> primes = new ConcurrentBag<long>();
+
+            Parallel.For(2L, 2000000L, l =>
+            {
+                if (Primes.IntegerIsPrime(l))
+                {
+                    primes.Add(l);
+                }
+            });
+
+            // Sum them
+            long sumOfPrimes = primes.Sum();
+
+            // Display
+            Console.WriteLine("Sum is: {0}", sumOfPrimes);
+
+            Console.Read();
+        }
+    }
+}
