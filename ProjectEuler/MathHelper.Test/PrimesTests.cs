05df909cfdb852dcc7dfc1f0b32ea1921c37d2c9
Daniel Mallott <daniel.mallott@gmail.com>
2016-02-24 10:24:28 -0600

    Adding a MathHelper class and tests.

---
+++ ProjectEuler/MathHelper.Test/PrimesTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathHelper.Test
+{
+    [TestClass]
+    public class PrimesTests
+    {
+        [TestMethod]
+        public void Test_IntegerIsPrime_Two()
+        {
+            bool result = Primes.IntegerIsPrime(2);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_IntegerIsPrime_Three()
+        {
+            bool result = Primes.IntegerIsPrime(3);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_IntegerIsPrime_Four()
+        {
+            bool result = Primes.IntegerIsPrime(4);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_IntegerIsPrime_Five()
+        {
+            bool result = Primes.IntegerIsPrime(5);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Test_IntegerIsPrime_TwentySeven()
+        {
+            bool result = Primes.IntegerIsPrime(27);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Test_IntegerIsPrime_NinetySeven()
+        {
+            bool result = Primes.IntegerIsPrime(97);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ ProjectEuler/MathHelper/Primes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathHelper
+{
+    public class Primes
+    {
+        /// <summary>
+        /// Determine whether the passed <paramref name="numberToTest"/> is prime or not.
+        /// </summary>
+        /// <param name="numberToTest">The natural number to determine if prime.</param>
+        /// <returns><c>true</c> if prime; otherwise, <c>false</c>.</returns>
+        /// <remarks>
+        /// Uses the brute force method of dividing the <paramref name="numberToTest"/> (n) by
+        /// a sequence of number (2, 3, 4,..., sqrt(n)).
+        /// </remarks>
+        public static bool IntegerIsPrime(long numberToTest)
+        {
+            bool isPrime = true;
+
+            if(numberToTest == 2)
+            {
+                return isPrime;
+            }
+
+            // Determine the max number for our division sequence.
+            var squareRoot = Math.Sqrt(numberToTest);
+
+            long maxDivisor = Convert.ToInt64(Math.Ceiling(squareRoot));
+
+            // Create our sequence
+            List<long> divisors = new List<long>();
+            for (long l = 2; l <= maxDivisor; l++)
+            {
+                divisors.Add(l);
+            }
+
+            // Test the number
+            foreach(long divisor in divisors)
+            {
+                if(numberToTest % divisor == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }            
+
+            return isPrime;
+        }
+    }
+}
